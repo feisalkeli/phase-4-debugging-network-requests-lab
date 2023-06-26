0d3bca25918865d72f6bd1124cc6410785188bed
FeisalKeli <kellyfeisal2000@gmail.com>
2023-06-26 09:57:33 +0300

    lab done

--- app/controllers/toys_controller.rb
+++ app/controllers/toys_controller.rb
@@ -7,13 +7,18 @@ class ToysController < ApplicationController
   end
 
   def create
-    toy = Toys.create(toy_params)
-    render json: toy, status: :created
+   toy = Toy.create(toy_params)
+   if toy 
+    render json:toy, status: :created
+   else
+    render json: {errors: toy.errors.full_messages}, status: :unprocessable_entity
+   end
   end
 
   def update
     toy = Toy.find_by(id: params[:id])
     toy.update(toy_params)
+    render json:toy, status: :created
   end
 
   def destroy
